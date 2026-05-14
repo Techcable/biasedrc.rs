f98e194eefbc7a3a0648b96f8fdb63e8d671279e
Techcable <git@techcable.net>
2026-05-13 20:06:18 -0700

    bench: Use SharedPtrKind trait instead of macro
    Cleaner and more flexible.

--- benchmark/basic.rs
+++ benchmark/basic.rs
@@ -1,44 +1,65 @@
 #![allow(clippy::disallowed_types)]
 use biasedrc::{BiasedCountError, Brc};
+use criterion::Criterion;
 use std::rc::Rc;
 use std::sync::Arc;
 use triomphe::Arc as ArcT;
 
-macro_rules! bench_new {
-    ($c:ident => $($target:ident),*) => {
-        $($c.bench_function(concat!(stringify!($target), "::new"), |b| {
-            b.iter_with_large_drop(|| $target::new(7));
-        });)*
-    };
+/// Abstracts over different types of recounted pointers.
+///
+/// Similar to [`archery::SharedPointerKind`].
+///
+/// [archery `SharedPointerKind`]: https://docs.rs/archery/1.2/archery/shared_pointer/kind/trait.SharedPointerKind.html
+trait SharedPtrKind {
+    type Ref<T>: Sized + Clone;
+    fn bench_name(op: &str) -> String;
+    fn new<T>(value: T) -> Self::Ref<T>;
+}
+mod kinds {
+    use super::SharedPtrKind;
+    macro_rules! simple_shared_ptr {
+        ($($target:ident),+ $(,)?) => {$(
+            pub struct $target;
+            impl SharedPtrKind for $target {
+                type Ref<T> = super::$target::<T>;
+                fn bench_name(op: &str) -> String {
+                    format!("{tp}::{op}", tp = stringify!($target))
+                }
+                #[inline]
+                fn new<T>(value: T) -> Self::Ref<T> {
+                    super::$target::new(value)
+                }
+            }
+        )*};
+    }
+    simple_shared_ptr!(Arc, ArcT, Rc, Brc, BrcShared);
 }
 
-macro_rules! bench_clones {
-    ($c:ident => $($target:ident),*) => {
-        $($c.bench_function(concat!(stringify!($target), "::clone"), |b| {
-            let rc = $target::new(7);
-            b.iter_with_large_drop(|| $target::clone(&rc));
-        });)*
-    };
+fn bench_new<K: SharedPtrKind>(c: &mut Criterion) {
+    c.bench_function(&K::bench_name("new"), |b| {
+        b.iter_with_large_drop(|| K::new(7));
+    });
 }
 
-macro_rules! bench_drops {
-    ($c:ident => $($target:ident),*) => {
-        $($c.bench_function(concat!(stringify!($target), "::drop_unique"), |b| {
-            b.iter_batched(
-                || $target::new(7),
-                drop,
-                criterion::BatchSize::SmallInput,
-            )
-        });
-        $c.bench_function(concat!(stringify!($target), "::drop_shared"), |b| {
-            let rc = $target::new(7);
-            b.iter_batched(
-                || $target::clone(&rc),
-                drop,
-                criterion::BatchSize::SmallInput,
-            )
-        });)*
-    };
+fn bench_clone<K: SharedPtrKind>(c: &mut Criterion) {
+    let rc = K::new(7);
+    c.bench_function(&K::bench_name("clone"), |b| {
+        b.iter_with_large_drop(|| K::Ref::clone(&rc));
+    });
+}
+
+fn bench_drop<K: SharedPtrKind>(c: &mut Criterion) {
+    c.bench_function(&K::bench_name("drop_unique"), |b| {
+        b.iter_batched(|| K::new(7), drop, criterion::BatchSize::SmallInput);
+    });
+    c.bench_function(&K::bench_name("drop_shared"), |b| {
+        let rc = K::new(7);
+        b.iter_batched(
+            || K::Ref::clone(&rc),
+            drop,
+            criterion::BatchSize::SmallInput,
+        );
+    });
 }
 
 /// A wrapper around a [`Brc`] which only affects the shared reference count.
@@ -61,8 +82,8 @@ impl<T> BrcShared<T> {
     /// The most important reason that this function cannot be benchmarked
     /// is that it requires a sequence of clone/drop operations.
     /// This is because we do not currently offer a proper `Brc::new_shared` function,
-    /// so have to emulate it with a good deal of overhead.
-    /// This overhead is fine if we keep this function
+    /// so we have to emulate it with a good deal of overhead.
+    /// This overhead is fine if we restrict this function to the initial setup.
     #[allow(clippy::missing_panics_doc)]
     pub fn new(value: T) -> Self {
         let biased = Brc::new(value);
@@ -81,10 +102,20 @@ impl<T> Clone for BrcShared<T> {
     }
 }
 
-pub fn main() {
-    let mut c = criterion::Criterion::default().configure_from_args();
+macro_rules! bench_all {
+    ($c:ident, ops: [], kinds: $($ignored:tt)*) => ({});
+    ($c:ident, ops: [$op:ident $(, $op_extra:ident)*], kinds: $($kind:ident),+) => ({
+        paste3::paste! {
+            $([<bench_ $op>]::<kinds::$kind>(&mut $c);)*
+        }
+        // recurse for remaining ops. this is done to avoid capture iteration problems
+        bench_all!($c, ops: [$($op_extra),*], kinds: $($kind),*)
+    })
+}
 
-    bench_new!(c => Arc, ArcT, Brc, Rc);
-    bench_clones!(c => Arc, ArcT, Brc, BrcShared, Rc);
-    bench_drops!(c => Arc, ArcT, Brc, BrcShared, Rc);
+pub fn main() {
+    let mut c = Criterion::default().configure_from_args();
+
+    bench_all!(c, ops: [new], kinds: Arc, ArcT, Brc, Rc);
+    bench_all!(c, ops: [clone, drop], kinds: Arc, ArcT, Brc, BrcShared, Rc);
 }
